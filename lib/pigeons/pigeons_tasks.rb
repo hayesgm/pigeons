ec681491956f4f45b7c646dbe48b2edca87a619b
Geoff Hayes <hayesgm@gmail.com>
2013-03-14 15:25:05 -0700

    add pigeon rake tasks

--- lib/pigeons/pigeons_tasks.rb
+++ lib/pigeons/pigeons_tasks.rb
@@ -1,6 +1,8 @@
 
-class PigeonTask < Rails::Railtie
-  rake_tasks do
-    Dir[File.join(File.dirname(__FILE__),'tasks/*.rake')].each { |f| load f }
+if defined?(Rails)
+  class PigeonTask < Rails::Railtie
+    rake_tasks do
+      Dir[File.join(File.dirname(__FILE__),'../../tasks/*.rake')].each { |f| load f }
+    end
   end
 end
